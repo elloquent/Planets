ae861faeb833bc008382eeaf42174d30fd036baf
Richard Elridge <richardelridge78@outlook.com>
2020-07-13 10:31:55 -0500

    All classes and method created

--- Planets/Program.cs
+++ Planets/Program.cs
@@ -6,6 +6,18 @@ namespace Planets
     {
         static void Main(string[] args)
         {
+            giantplanet _giantplanet = new giantplanet("gas", 10.4, 20, "pluto");
+            _giantplanet.ToString();
+            terrestialplanet _terrestialplanet = new terrestialplanet(true, 15, 30, "Earth");
+            _terrestialplanet.ToString();
         }
+
+        public void waitforanykey()
+        {
+
+            Console.ReadKey();
+
+        }
+    }
     }
 }
